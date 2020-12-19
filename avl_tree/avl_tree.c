e42bcdc6fd32d74c98af26f54685e6403ac3c90e
Danijel Jovanović <danijel.xda@gmail.com>
2020-12-19 17:35:07 +0100

    Third case is

--- avl_tree/avl_tree.c
+++ avl_tree/avl_tree.c
@@ -80,6 +80,11 @@ STABLO *AVL_dodaj_u_stablo(STABLO *cvor, int broj)
     
     if(blc < -1 && broj > cvor -> desni -> broj)
         return levaRotacija(cvor);
+    
+    if(blc > 1 && broj > cvor -> levi -> broj) {
+       cvor -> levi = levaRotacija(cvor -> levi);    
+       return desnaRotacija(cvor);
+    }
 
     return cvor;
 }
