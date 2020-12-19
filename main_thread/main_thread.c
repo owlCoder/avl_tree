90978a57f0a6a3963bf6bca62c0ac2deb1a83cce
Danijel Jovanović <danijel.xda@gmail.com>
2020-12-19 18:17:11 +0100

    Make main_thread__stdin_check(...) works

--- main_thread/main_thread.c
+++ main_thread/main_thread.c
@@ -2,10 +2,14 @@
 
 void main_thread__stdin_check(const unsigned argc)
 {
-
+    if(argc < 2)
+    {
+        printf("\nUSAGE: balansirano_binarno_stablo num1 ...\n");
+        exit(42);
+    }
 }
 
-void main_thread__data_load(const unsigned, const char **)
+void main_thread__data_load(const unsigned argc, const char **argn)
 {
     //
 }
