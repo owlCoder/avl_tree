5b1e2a71e8f1a5c79012130aa81e6d8570308d9d
Данијел Јовановић <jovanovic.pr55.2020@uns.ac.rs>
2020-12-19 20:18:11 +0100

    Refactor code of main

--- binarno_stablo.c
+++ binarno_stablo.c
@@ -1,27 +1,12 @@
-#include "avl_tree/avl_tree.h"
+#include "modules/main_thread/main_thread.h"
 
 int main(int argc, char **argn)
-{
-    STABLO *koren;
+{   
+    STABLO *koren, *avl_koren;
 
-    init_stablo(&koren);
-
-    if(argc < 2)
-    {
-        printf("\nUSAGE: %s num1 ...\n", argn[0]);
-        exit(42);
-    }
-    unsigned brarg = argc - 1, i = 1;
-    while(i <= brarg)
-        dodaj_u_stablo(kreiraj_element(atoi(argn[i++])), &koren);
-
-    // printf("\n"); prikaz_stabla(koren); printf("\n\n");
-
-    int levo = dubina_levog_podstabla(koren);
-    int desno = dubina_desnog_podstabla(koren);
-
-    print_tree(levo, desno, "Pre");
-    print_tree(levo, desno, "Posle");
-
+    main_thread__stdin_check(argc);    
+    main_thread__data_load(argc, argn, &koren, &avl_koren);
+    main_thread__stdout_print(koren, avl_koren);
+    
     return 0;
 }
